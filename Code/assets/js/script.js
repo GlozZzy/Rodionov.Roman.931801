48f2a4fa415ff06830234cae5c8fa104d96d5d2b
Roman Rodionov <kpackl14@gmail.com>
2020-11-26 19:17:54 +0700

    Final change Lab9

--- Code/assets/js/script.js
+++ Code/assets/js/script.js
@@ -13,20 +13,18 @@ function print_z(z) {
 
 
 function print_n(n) {
-
-
     var inp = document.getElementById('input_grey').textContent.split(' ');
     if (inp[inp.length - 1].length == 1 && inp[inp.length - 1][0] === '0') {
         document.getElementById('input_grey').textContent = document.getElementById('input_grey').textContent.slice(0, -1) + n;
         document.getElementById('input_black').textContent = document.getElementById('input_black').textContent.slice(0, -1) + n;
     }
     else {
-        if (document.getElementById('input_black').textContent[0] == ' ') {
+        let t = document.getElementById('input_grey').textContent.split(' ');
+        if (t[t.length-1] == '') {
             document.getElementById('input_black').textContent = document.getElementById('input_black').textContent.slice(2, -1);
         }
         document.getElementById('input_grey').textContent += n;
         document.getElementById('input_black').textContent += n;
- 
     }
 }
 
@@ -47,41 +45,51 @@ function clear_(elem) {
 
 
 function cancel_() {
-    if (document.getElementById('input_grey').textContent.length != 1) {
-        if (document.getElementById('input_grey').textContent[document.getElementById('input_grey').textContent.length - 1] == ' ') {
-            document.getElementById('input_grey').textContent = document.getElementById('input_grey').textContent.slice(0, -3);
-            document.getElementById('input_black').textContent = document.getElementById('input_black').textContent.slice(0, -3);
+    let gre = document.getElementById('input_grey');
+    let blk = document.getElementById('input_black');
+
+    if (gre.textContent.length != 1) {
+        if (gre.textContent[gre.textContent.length - 1] == ' ') {
+            gre.textContent = gre.textContent.slice(0, -3);
+            blk.textContent = blk.textContent.slice(0, -3);
         }
         else {
-            document.getElementById('input_grey').textContent = document.getElementById('input_grey').textContent.slice(0, -1);
-            document.getElementById('input_black').textContent = document.getElementById('input_black').textContent.slice(0, -1);
+            gre.textContent = gre.textContent.slice(0, -1);
+            blk.textContent = blk.textContent.slice(0, -1);
         }
     }
     else {
-        document.getElementById('input_grey').textContent = '0';
-        document.getElementById('input_black').textContent = '0';
+        gre.textContent = '0';
+        blk.textContent = '0';
     }
 
-    let t = document.getElementById('input_grey').textContent.split(' ');
-    if (document.getElementById('input_black').textContent == '') {
-        document.getElementById('input_black').textContent = t[t.length - 1];
+    let t = gre.textContent.split(' ');
+    if (blk.textContent == '') {
+        blk.textContent = t[t.length - 1];
+    }
+
+    if (blk.textContent == '' && t[t.length - 1] == '') {
+        blk.textContent = t[t.length - 2] + " ";
     }
 }
 
 
 function solve_() {
     var inp = document.getElementById('input_grey').textContent.split(' ');
-    if (inp[inp.length - 1]) {
+    if (inp[inp.length - 1] === '') {
+        var result = eval(document.getElementById('input_grey').textContent.slice(0, -3)) 
+    }
+    else {
         var result = eval(document.getElementById('input_grey').textContent)
+    }
 
+    if (result != Infinity && result != -Infinity) {
         document.getElementById('input_grey').textContent = (parseInt(result * 10000)) / 10000;
         document.getElementById('input_black').textContent = (parseInt(result * 10000)) / 10000;
     }
     else {
-        var result = eval(document.getElementById('input_grey').textContent.slice(0, -3))
-
-        document.getElementById('input_grey').textContent = (parseInt(result * 10000)) / 10000;
-        document.getElementById('input_black').textContent = (parseInt(result * 10000)) / 10000;
+        document.getElementById('input_grey').textContent = result;
+        document.getElementById('input_black').textContent = result;
     }
 }
 
@@ -94,5 +102,5 @@ cl.addEventListener('mousedown', event => {
 })
 
 cl.addEventListener('mouseup', event => {
-    clearInterval(interval)
+    clearInterval(interval);
 })
